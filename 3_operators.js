10066337df76d257ed1b7c13dc0dad189ab159ec
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2020-09-17 15:02:50 +0530

    Basic operators: Math, Logical, typeof

---
+++ 3_operators.js
@@ -0,0 +1,36 @@
+// Basic Operators
+
+var year, yearAmru, yearJohn;
+now = 2020;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - 28
+yearAmru = now - 19
+
+console.log(now + 2);
+console.log(now * 2); 
+console.log(now - 2); 
+console.log(now / 10); 
+
+// Logical operators
+var johnOlder = ageJohn > ageMark;
+
+// returns true/false
+console.log(johnOlder);
+
+//typeof operator
+
+//returns boolean
+console.log(typeof johnOlder);
+
+//returns number
+console.log(typeof ageJohn);
+
+//returns string
+console.log(typeof "Mark is older than John");
+
+//returns undefined
+var x;
+console.log(typeof x);
