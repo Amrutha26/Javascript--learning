2032aee33896145518d7df260ec656dd518738b7
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2020-09-17 13:43:11 +0530

    Variables and Datatypes

---
+++ 1_var.js
@@ -0,0 +1,37 @@
+// Variable : Container in which we can store the value in order to use it over and over again in our code.
+
+// Declare a variable
+var firstName = "Amru";
+
+// Prints the firstName in the console
+console.log(firstName);
+
+var lastName = "Amudhan";
+var age = 19;
+
+// Primitive Javascript Data Types:
+
+// Number: floating point numbers, for decimals and integers
+// String: Sequence of characters, used for text
+// Boolean: logical datatype: true or false
+// Undefined: Datatype of a variable that does not hava a value yet
+// Null: Non-existent
+
+// Javascript has dynamic typing: the interpreter assigns variables a type at runtime based on the variable's value at the time.
+
+// Boolean
+var fullAge = true;
+console.log(fullAge);
+
+//Convention to use camelcasing
+
+//Undefined: just declare and not assign
+var job;
+console.log(job);
+
+job = "Teacher";
+console.log(job);
+
+// Rules to name a variable:
+// Variables cannot start with numbers or symbols. (Exceptions: $ _ )
+// Cannot use reserves javascript keywords for variable names
