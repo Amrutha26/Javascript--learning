cdfbdb4eb1e1ae8e1a634ccaf23a3fd6b7a8d113
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2020-10-10 20:41:57 +0530

    :six: If else statements

---
+++ 06_ifelse.js
@@ -0,0 +1,18 @@
+// If else statements
+
+var firstName = 'John';
+var civilStatus = 'married';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+
+var isMarried = false;
+//isMarried === true is not needed as isMarried is already a boolean value and is true by default
+if (isMarried === 'married') {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
