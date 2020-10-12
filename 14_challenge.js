161f44acaab5b4aebf1c95875df172d415b9c43d
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2020-10-12 13:07:41 +0530

    :one: :four: Coding Challenge 3

---
+++ 14_challenge.js
@@ -0,0 +1,17 @@
+function tipCalculator(bill) {
+    var percentage;
+    if(bill < 50 ) {
+        percentage = 0.2;
+    } else if(bill >= 50 && bil < 200) {
+        percentage = .15;
+    } else {
+        percentage = .1;
+    }
+    return percentage * bill;
+}
+
+var bills = [124, 48, 268];
+var tips = [tipCalculator(bills[0]), tipCalculator(bills[1]),tipCalculator(bills[2])];
+var finalValues = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(tips, finalValues);
