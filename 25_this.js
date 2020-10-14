53a3d180437ffef3e97cf1ff5030d048f5eebb92
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2020-10-14 11:57:12 +0530

    :two: :five: This keyword

---
+++ 25_this.js
@@ -0,0 +1,6 @@
+/*This variable - each and every execution context gets
+Stored in the execution context object
+Regular function call: this keyword points at the global object (window object) - DEFAULT
+Method call: this variable points to the object that is calling the method
+The this keyowrd is not assigned a value until a function where it is defined is actually called.
+This keyword is attached to an execution context which is only created as soon as the function is invoked/called */
