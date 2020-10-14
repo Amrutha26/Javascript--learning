0198aa3e85456e5871691b27447a87d932edea78
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2020-10-14 10:10:22 +0530

    :two: :one: Execution Contexts and Execution Stack

---
+++ 21_exec.js
@@ -0,0 +1,5 @@
+//All JS codes run in an environment -> Execution Context (Piece of code is executed )-> Default - Global Execution Context (Code that is not inside any function)
+// You can also think of the global execution context as an object -> In the browser, that's the window object
+// lastName === window.lastName
+//Global execution context lies as the first one in the execution stack.
+//On top of the global execution context, as many new execution context (function) lies on the gec - EXECUTION STACK
