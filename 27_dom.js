114a408e19976051454735c0e72ac13652c207b9
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2020-11-02 22:50:57 +0530

    :two: :seven DOM and DOM manipulation

---
+++ 27_dom.js
@@ -0,0 +1,8 @@
+/*DOM - Document Object Model
+Strucutred representation of an HTML document
+Connect webpages to scripts
+For each HTML box, there is an object in the DOM that we can access and interact with. 
+Javascript and DOM are different
+Use methods (functions attached to some object) / functions in javascript that allows us to interact and manipulare the DOM and therefore the webpage
+functions attached to some object - this is the object that gives access to the DOM
+The html webpage content is stored in the DOM, which can then be accessed and manipulated by Javascript */ 
